5f9b1cc7fc560a0196cc6ebecbfd8d0a72d3f3cb
Nidhi-Data-Analyst <guptanidhi1993@gmail.com>
2024-06-19 11:47:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generateSignature() {
         const linkedinHtml = linkedin ? `
             <div class="vertical-line"></div>
             <a href="${linkedin}" target="_blank">
-                <img src="linkedin-icon.png" alt="LinkedIn" class="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/linkedin-logo">
+                <img src="linkedin-icon.png" alt="LinkedIn" class="linkedin-logo">
             </a>
         ` : '';
 
@@ -21,7 +21,7 @@ function generateSignature() {
             <div class="signature-container">
                 <div class="left-column">
                     <img src="${profilePicBase64}" alt="Profile Picture" class="profile-pic">
-                    <img src="school-logo.png" alt="School Logo" class="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/school-logo">
+                    <img src="school-logo.png" alt="School Logo" class="school-logo">
                     <div class="school-locations">
                         <span style="font-weight: ${campus === 'Noida' ? 'bold' : 'normal'};">Noida</span> | 
                         <span style="font-weight: ${campus === 'Gurgaon' ? 'bold' : 'normal'};">Gurgaon</span> | 
@@ -39,24 +39,24 @@ function generateSignature() {
                     <span class="designation" id="designation">${designation}</span>
                     <div class="contact-info">
                         <div>
-                            <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/phone-icon.png" alt="Phone Icon"> 
+                            <img src="phone-icon.png" alt="Phone Icon"> 
                             <a href="tel:${phone}" id="phone">${phone}</a>
                         </div>
                         <div>
-                            <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/email-icon.png" alt="Email Icon"> 
+                            <img src="email-icon.png" alt="Email Icon"> 
                             <a href="mailto:${email}" id="email">${email}</a>
                         </div>
                         <div>
-                            <img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/website-icon.png" alt="Web Icon"> 
+                            <img src="website-icon.png" alt="Web Icon"> 
                             <a href="https://shivnadarschool.edu.in/" id="website">https://shivnadarschool.edu.in/</a>
                         </div>
                     </div>
                     <div class="social-icons">
-                        <a href="https://www.facebook.com/shivnadarschool"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/facebook-icon.png" alt="Facebook"></a>
-                        <a href="https://www.instagram.com/shivnadarschool"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/instagram-icon.png" alt="Instagram" class="instagram"></a>
-                        <a href="https://www.youtube.com/shivnadarschool"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/youtube-icon.png" alt="YouTube"></a>
-                        <a href="https://www.linkedin.com/school/shiv-nadar-school/"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/linkedin-icon.png" alt="LinkedIn"></a>
-                        <a href="https://www.twitter.com/shivnadarschool"><img src="https://github.com/Nidhi-Data-Analyst/Email-Signature/blob/main/twitter-icon.png" alt="Twitter"></a>
+                        <a href="https://www.facebook.com/shivnadarschool"><img src="facebook-icon.png" alt="Facebook"></a>
+                        <a href="https://www.instagram.com/shivnadarschool"><img src="instagram-icon.png" alt="Instagram" class="instagram"></a>
+                        <a href="https://www.youtube.com/shivnadarschool"><img src="youtube-icon.png" alt="YouTube"></a>
+                        <a href="https://www.linkedin.com/school/shiv-nadar-school/"><img src="linkedin-icon.png" alt="LinkedIn"></a>
+                        <a href="https://www.twitter.com/shivnadarschool"><img src="twitter-icon.png" alt="Twitter"></a>
                     </div>
                 </div>
             </div>
